99be63fe40d7e31167d796f2b043cd1815c08cb3
Vanessa Schneider <vanessajwade@gmail.com>
2022-01-16 13:24:11 -0500

    commit tree f209ea2fe60c3e946d5c88c704876db093254347

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+
+
+  
+  
+
+ 
+
+function findMatching(array, string) {
+	return array.filter(name=> name.toLowerCase() === string.toLowerCase())
+}
+
+
+
+
+
+function fuzzyMatch (array, string) {
+return array.filter(name=>name.slice(0,2) === string.slice(0,2))
+
+}
+
+function matchName(array, string) {
+
+return array.filter(element=>element.name === string)
+
+
+}
